ded7950d7c66958206012a51b594224a4d2b5ff6
Sergio Salazar Williams <sawsdevx8@gmail.com>
2024-03-06 08:10:59 -0500

    Add phonebook backend step 10
    Adjust port channeling
    
    Add README.md with the deployed app link

---
+++ README.md
@@ -0,0 +1,3 @@
+Backend Phonebook App
+You can found the backend published here:
+[Link](https://sawsfso2023-phonebook-backend.fly.dev/) 
--- index.js
+++ index.js
@@ -95,6 +95,6 @@ app.get('/api/info', (request, response) => {
 })
 
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT)
 console.log(`Persons server runing on ${PORT}`);
