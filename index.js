4999b225f12870755a766be686435f9153d09ab7
Sergio Salazar Williams <sawsdevx8@gmail.com>
2024-03-06 07:47:34 -0500

    Add phonebook backend step 9

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
 const app = express()
 let persons = [
     { 
@@ -25,6 +26,7 @@ let persons = [
 ]
 morgan.token('body', function (req, res) { return JSON.stringify(req.body) })
 
+app.use(cors())
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
