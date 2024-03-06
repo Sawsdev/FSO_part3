111e05a30a30a89acb33ea438c082c3236883b3b
Sergio Salazar Williams <sawsdevx8@gmail.com>
2024-03-06 06:37:03 -0500

    Add Part 3 step 7 & 8

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require('express')
+const morgan = require('morgan')
 const app = express()
-app.use(express.json())
-
 let persons = [
     { 
       "id": 1,
@@ -24,6 +23,11 @@ let persons = [
       "number": "39-23-6423122"
     }
 ]
+morgan.token('body', function (req, res) { return JSON.stringify(req.body) })
+
+app.use(express.json())
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
+
 
 app.get('/', (request, response) => {
     response.status(200).send("<h1>Welcome to persons phonebook</h1>")
